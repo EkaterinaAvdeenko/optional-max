3133565b8a719e180c4918ea6b9bb54ba9507b56
Avdeenko Ekaterina <avdeenqoeka@gmail.com>
2023-12-17 21:48:02 +0100

    optional-max_AvdeenkoEkaterina

--- src/main/java/com/epam/rd/autotasks/max/MaxMethod.java
+++ src/main/java/com/epam/rd/autotasks/max/MaxMethod.java
@@ -1,20 +1,22 @@
 package com.epam.rd.autotasks.max;
+
 import java.util.OptionalInt;
 
 public class MaxMethod {
-    public static OptionalInt max(int[] values) {
-        OptionalInt result;
-        if(values != null && values.length !=0){
-            int max = values[0];
-            for(int i = 0; i< values.length; i++){
-                if(values[i] > max)
-                    max = values[i];
-            }
-            result = OptionalInt.of(max);
-        }
-        else{
-            result = OptionalInt.empty();
-        }
-        return result;
+
+  public static OptionalInt max(int[] values) {
+    OptionalInt result;
+    if (values != null && values.length != 0) {
+      int max = values[0];
+      for (int i = 0; i < values.length; i++) {
+          if (values[i] > max) {
+              max = values[i];
+          }
+      }
+      result = OptionalInt.of(max);
+    } else {
+      result = OptionalInt.empty();
     }
+    return result;
+  }
 }
